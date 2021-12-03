ea13f33f36e24989429c921e67020f7da6ea635c
Dick van Viegen <dick@DICK-DESKTOP.localdomain>
2021-12-03 11:47:46 +0100

    Added Day3 solution

---
+++ src/Y2021/Day3.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace marvellanius\Advent\Y2021;
+
+use marvellanius\Advent\Answer;
+use marvellanius\Advent\Day;
+use phpDocumentor\Reflection\Types\Callable_;
+
+/**
+ * @author Dick van Viegen <dick@tweakers.net>
+ */
+class Day3 extends Day
+{
+    public function run1(): Answer
+    {
+        $array = $this->convertToPositionArray(array_map(static fn ($value) => trim($value), $this->getInputAsArray()));
+
+        $gamma = implode(array_map([$this, 'determineMostCommonValue'], $array));
+        $epsilon = implode(array_map([$this, 'determineLeastCommonValue'], $array));
+
+        return new Answer((string) (bindec($gamma) * bindec($epsilon)));
+    }
+
+    public function run2(): Answer
+    {
+        $array = array_map(static fn ($value) => trim($value), $this->getInputAsArray());
+
+        $ogr = $this->determineRating($array, strlen($array[0]), 'determineMostCommonValue');
+        $csr = $this->determineRating($array, strlen($array[0]), 'determineLeastCommonValue');
+
+        return new Answer((string) (bindec($ogr) * bindec($csr)));
+    }
+
+    private function determineRating(array $array, int $length, string $callable): string
+    {
+        for ($key = 0; $key < $length; $key++) {
+            if (count($array) > 1) {
+                $array = $this->filterArrayForValues($array, $this->$callable($this->convertToPositionArray($array)[$key]), $key);
+            }
+        }
+        return array_values($array)[0];
+    }
+
+    private function filterArrayForValues(array $array, int $value, int $key): array
+    {
+        return array_filter($array, static fn ($item) => (int) $item[$key] === $value);
+    }
+
+    private function convertToPositionArray(array $array): array
+    {
+        $positionArray = [];
+
+        $binArray = array_map(static fn ($value) => str_split($value), $array);
+
+        foreach ($binArray as $key => $bin) {
+            foreach ($bin as $k => $v) {
+                $positionArray[$k][$key] = $v;
+            }
+        }
+
+        return array_map(static fn ($v) => array_count_values($v), $positionArray);;
+    }
+
+    private function determineMostCommonValue(array $array): int
+    {
+        return array_values($array)[0] === array_values($array)[1] ? 1 : array_keys($array, max($array), true)[0];
+    }
+
+    private function determineLeastCommonValue(array $array): int
+    {
+        return array_values($array)[0] === array_values($array)[1] ? 0 : array_keys($array, min($array), true)[0];
+    }
+}
---
+++ test/unit/Y2021/Day3Test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace marvellanius\Advent\Y2021;
+
+use marvellanius\Advent\Answer;
+use marvellanius\Advent\Day;
+use marvellanius\Advent\DayTestCase;
+
+final class Day3Test extends DayTestCase
+{
+    protected function getDay(): Day
+    {
+        $day = new Day3();
+        $day->setInput(dirname(__DIR__) . "/../../resources/Y2021/Day3-test.txt");
+        return $day;
+    }
+
+    protected function getExpectedAnswerForAssignment(int $assignment): Answer
+    {
+        return match($assignment) {
+            1 => new Answer(198),
+            2 => new Answer(230)
+        };
+    }
+}
