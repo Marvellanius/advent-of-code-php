fe5d454203322b73b1e54bc695c10d2a629a3332
Dick van Viegen <dick.van.viegen@tweakers.net>
2021-12-08 19:52:50 +0100

    Sped up Day1

--- src/Y2021/Day1.php
+++ src/Y2021/Day1.php
@@ -10,7 +10,7 @@ final class Day1 extends AbstractDay
     public function run1(): Answer
     {
         $array = array_map(static fn ($value) => trim($value), $this->getInputAsArray());
-        
+
         return new Answer($this->getComparisonSuccessCount($array, 'isGreaterThanPrevious'));
     }
 
@@ -19,15 +19,31 @@ final class Day1 extends AbstractDay
         $array = array_map(static fn ($value) => trim($value), $this->getInputAsArray());
         $windowLength = 3;
 
-        $arrayOfSums = [];
-        for ($key = 0; $key <= count($array) - $windowLength; $key++) {
-            $arrayOfSums[] = array_reduce(
-                array_slice($array, $key, $windowLength), 
-                static fn ($carry, $item) => $carry += $item
-            );
+        // O(2n)?
+//        $arrayOfSums = [];
+//        for ($key = 0; $key <= count($array) - $windowLength; $key++) {
+//            $arrayOfSums[] = array_reduce(
+//                array_slice($array, $key, $windowLength),
+//                static fn ($carry, $item) => $carry += $item
+//            );
+//        }
+//        return new Answer($this->getComparisonSuccessCount($arrayOfSums, 'isGreaterThanPrevious'));
+
+        // O(n)
+        $previousSum = array_sum(array_slice($array, 0, $windowLength));
+        $windowSum = $previousSum;
+
+        $greaterThanCount = 0;
+        for ($i = $windowLength; $i < count($array); $i++)
+        {
+            $windowSum += $array[$i] - $array[$i - $windowLength];
+            if ($windowSum > $previousSum) {
+                $greaterThanCount++;
+            }
+            $previousSum = $windowSum;
         }
 
-        return new Answer($this->getComparisonSuccessCount($arrayOfSums, 'isGreaterThanPrevious'));
+        return new Answer((string) $greaterThanCount);
     }
 
     private function getComparisonSuccessCount(array $array, string $callback): int
